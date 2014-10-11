1d59c51eece6a1c41b7e2f5bfaec1cb28ca9337c
crisscrosskao <crisscrosskao@gmail.com>
2014-10-11 18:30:38 -0400

    Depth Map test

---
+++ Kinect test/src/Kinect/DepthMap.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Kinect;
+
+/**
+ *
+ * @author crisscrosskao
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Iterator;
+import SimpleOpenNI.*;
+import processing.core.*;
+import static processing.core.PApplet.println;
+
+public class DepthMap extends PApplet 
+{
+  public DepthMap() 
+  {
+      SimpleOpenNI context = new SimpleOpenNI (this);
+      ContextTest = context;
+  }
+  public static void main(String args[]) 
+  {
+    PApplet.main(new String[] {"Kinect.DepthMap"});
+  }
+  
+  private SimpleOpenNI ContextTest;
+  
+  public void setup() 
+  {
+    size(640*2, 480);
+    
+    if (ContextTest.isInit() == false)
+    {
+        println("Can't init SimpleOpenNI, maybe the camera is not connected!"); 
+        exit();
+        return;
+    }
+    ContextTest.setMirror(true);
+
+    // enable depthMap generation 
+    ContextTest.enableDepth();
+
+    // enable ir generation
+    ContextTest.enableRGB();
+  }
+
+  // mirror is by default enabled
+
+ public void draw() 
+  {
+    ContextTest.update();
+
+     background(200, 0, 0);
+
+  // draw depthImageMap
+    image(ContextTest.depthImage(), 0, 0);
+
+  // draw irImageMap
+    image(ContextTest.rgbImage(), ContextTest.depthWidth() + 10, 0);
+  }
+ 
+}
+
+
+
+
+
+
