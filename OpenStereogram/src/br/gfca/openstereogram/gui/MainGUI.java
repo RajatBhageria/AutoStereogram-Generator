ac34562ef0e4abb990271736fd48d61d0105fb0b
crisscrosskao <crisscrosskao@gmail.com>
2014-10-11 18:52:47 -0400

    added libraries to github file

--- OpenStereogram/src/br/gfca/openstereogram/gui/MainGUI.java
+++ OpenStereogram/src/br/gfca/openstereogram/gui/MainGUI.java
@@ -463,7 +463,7 @@ public class MainGUI extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 285, Short.MAX_VALUE)
+            .addGap(0, 348, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
